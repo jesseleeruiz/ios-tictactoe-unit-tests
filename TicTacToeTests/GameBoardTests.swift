2cdc2b80caa3549b6c6410f6d71c2a0f1293734a
Jesse Ruiz <jesseleeruiz@gmail.com>
2019-11-06 08:13:32 -1000

    Added a Unit Test making sure Marks can be placed properly.

--- TicTacToeTests/GameBoardTests.swift
+++ TicTacToeTests/GameBoardTests.swift
@@ -37,5 +37,27 @@ class GameBoardTests: XCTestCase {
         ///If you are gonig to be clever make sure you use the message portion of the method.
     }
     
-    
+    func testPlacingMarks() {
+        
+        var board = GameBoard()
+        
+        XCTAssertNoThrow(try board.place(mark: .o, on: (0, 0)))
+        XCTAssertEqual(board[(0, 0)], .o)
+        
+        XCTAssertNoThrow(try board.place(mark: .x, on: (2, 2)))
+        XCTAssertEqual(board[(2, 2)], .x)
+        
+        for x in 0..<3 {
+            for y in 0..<3 {
+                if x == 0 && y == 0 { continue }
+                if x == 2 && y == 2 { continue }
+                
+                XCTAssertNil(board[(x, y)], "The entry at (\(x), \(y)) was \(board[(x, y)]!) instead of nil")
+            }
+        }
+        
+        XCTAssertThrowsError(try board.place(mark: .o, on: (0, 0))) { (error) in
+            XCTAssertEqual(error as? GameBoardError, GameBoardError.invalidSquare)
+        }
+    }
 }
